1d3cd1f1f16a50e8be252f7d9ac9b9a88bcff967
kartik-gupta-ij <kartikgupta7267@gmail.com>
2022-06-22 10:53:45 +0530

    Create refactoredPow.cpp

---
+++ Recusion/refactoredPow.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int pow(int m, int n){
+    if(n==0){
+        return 1;
+    }else if(n%2==0){
+        return pow(m*m,n/2);
+    }
+    else{
+        return pow(m*m,(n-1)/2)*m;
+    }
+}
+
+int main(){
+    cout<< pow(2,9);
+}
