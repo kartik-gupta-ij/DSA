dacf9a50b35068c8f7121a20a5264f5799cfbe92
kartik-gupta-ij <kartikgupta7267@gmail.com>
2022-06-22 10:42:30 +0530

    factorial

--- Recusion/SumOfNNumber.cpp
+++ Recusion/SumOfNNumber.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int fun1(int n){if(n>0){return (fun1(n-1)+n);}return 0;}
+int fun1(int n)
+{
+    if (n > 0)
+    {
+        return (fun1(n - 1) + n);
+    }
+    return 0;
+}
 
-int main(){int x=5;printf("%d",fun1(x));return 0;}
+int main()
+{
+    int x = 5;
+    printf("%d", fun1(x));
+    return 0;
+}
